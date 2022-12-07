acca7c1255dcf1502b9f3fcbbb31194355211440
Lucas Matheus da Silva Peres <83469680+LuscaMath@users.noreply.github.com>
2022-12-07 17:25:51 -0300

    Add files via upload

---
+++ tudo/assets/js/scripte.js
@@ -0,0 +1,15 @@
+/*Transição dos elementos quando eles aparecem na tela*/
+
+const observer = new IntersectionObserver( entries => {
+    Array.from(entries).forEach(entry => {
+        if (entry.intersectionRatio >= 1) {
+            entry.target.classList.add('init-hidden-off')
+        }
+    })
+}, {
+    threshold: [0, .5, 1]
+})
+
+Array.from(document.querySelectorAll('.init-hidden')).forEach(element => {
+    observer.observe(element)
+})
